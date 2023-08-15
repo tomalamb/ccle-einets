8a89abce935af3a774db2100729fcbe123757ef9
TomALamb <tomlamb216@gmail.com>
2023-08-15 09:31:24 +0100

    Update bash script.

--- src/training.sh
+++ src/training.sh
@@ -14,7 +14,7 @@ export CUDA_VISIBLE_DEVICES=$1
 
 # Setup conda environment.
 echo "Setting up bash enviroment"
-CONDA_ENV_NAME=einet_-cle
+CONDA_ENV_NAME=einet-cle
 echo "Activating conda environment: ${CONDA_ENV_NAME}"
 conda activate ${CONDA_ENV_NAME}
 
